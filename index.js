63195cd57853751b603647a3fde3185046c49b28
Elijah Noble <elijahnobledev@gmail.com>
2022-05-21 20:32:41 -0700

    sucessfully implemented search selector

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
     const init = () => {
         const searchForm = document.getElementById('eldenForm');
+        const selected = document.getElementById('category');
+        const search = document.getElementById('search')
+
+        let searchCategory = '';
+
+        
+        selected.addEventListener('change', (event) => {
+            event.preventDefault();
+            searchCategory = event.target.value;
+            //search.placeholder = `Search for a ${searchCategory}`
+            document.querySelector("input").setAttribute('placeholder', `Search for ${searchCategory}`)
+            console.log("CHECK#1 " + searchCategory)
+        })
+
+
       
         searchForm.addEventListener('submit', (event) => {
           event.preventDefault();
@@ -7,31 +22,35 @@
       
           console.log(event.target)
           console.log(searched);
+          console.log("CHECK#2 " + searchCategory)
       
-          fetch(`https://eldenring.fanapis.com/api/creatures?name=${searched}`)
+          fetch(`https://eldenring.fanapis.com/api/${searchCategory}?name=${searched}`)
           .then(response => response.json())
           .then(object => { 
               console.log(object)
               object.data.forEach(e => {
-                const img = document.createElement('div');
-                img.innerHTML = `
-                        <p>${e.name}</p>
-                        <div></div>
-                        <img src=${e.image}
-                        <div></div>
-                        <p>${e.description}</p>
-                        <div></div>
-                        <div></div>`
+                const div = document.createElement('div');
+                div.innerHTML = `
+                        <h4>${e.name}</h4>
+                        <img src=${e.image}>
+                        <details>
+                            <summary>Details</summary>
+                            <p>${e.description}</p>
+                        </details>`
                 // let img = document.createElement('img');
                 // let p = document.createElement('p');
                 // img.src = e.image
                 // p.innerHTML = `<p> NAME: ${e.name} </p>`
-                document.querySelector(".gallery").appendChild(img);
-              })
-                              
-          });
+                document.querySelector(".gallery").appendChild(div);
+              })                              
+          })
+          .catch(error => {
+            alert("Error!");
+            let p = document.createElement('p');
+            p.innerHTML = `ERROR: ${error.message}`
+            document.body.appendChild(p);
+          })          
         });
-
       }
       
       document.addEventListener('DOMContentLoaded', init);
