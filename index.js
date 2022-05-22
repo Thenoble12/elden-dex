0cea608c7459d23f9a1e1aed5a7aeff66ecc0087
Elijah Noble <elijahnobledev@gmail.com>
2022-05-22 11:33:55 -0700

    Made more styling and functional improvments

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 
         let searchCategory = '';
 
+        window.addEventListener('DOMContentLoaded', (event) => {
+            console.log('DOM fully loaded and parsed');
+        });
+
         
         selected.addEventListener('change', (event) => {
             event.preventDefault();
@@ -12,6 +16,33 @@
             //search.placeholder = `Search for a ${searchCategory}`
             document.querySelector("input").setAttribute('placeholder', `Search for ${searchCategory}`)
             console.log("CHECK#1 " + searchCategory)
+
+            fetch(`https://eldenring.fanapis.com/api/${searchCategory}?limit=500`)
+          .then(response => response.json())
+          .then(object => { 
+              console.log(object)
+              object.data.forEach(e => {
+                const div = document.createElement('div');
+                div.innerHTML = `
+                        <h4>${e.name}</h4>
+                        <img src=${e.image}>
+                        <details>
+                            <summary>Details</summary>
+                            <p>${e.description}</p>
+                        </details>`
+                // let img = document.createElement('img');
+                // let p = document.createElement('p');
+                // img.src = e.image
+                // p.innerHTML = `<p> NAME: ${e.name} </p>`
+                document.querySelector(".gallery").appendChild(div);
+              })                              
+          })
+          .catch(error => {
+            alert("Error!");
+            let p = document.createElement('p');
+            p.innerHTML = `ERROR: ${error.message}`
+            document.body.appendChild(p);
+          })     
         })
 
 
