d79cea051e6ae751c53196b4dcf93e1397d2d363
Elijah Noble <elijahnobledev@gmail.com>
2022-05-22 15:50:37 -0700

    1.0 commit, will be making further improvments

--- index.js
+++ index.js
@@ -16,33 +16,14 @@
             //search.placeholder = `Search for a ${searchCategory}`
             document.querySelector("input").setAttribute('placeholder', `Search for ${searchCategory}`)
             console.log("CHECK#1 " + searchCategory)
-
-            fetch(`https://eldenring.fanapis.com/api/${searchCategory}?limit=500`)
-          .then(response => response.json())
-          .then(object => { 
-              console.log(object)
-              object.data.forEach(e => {
-                const div = document.createElement('div');
-                div.innerHTML = `
-                        <h4>${e.name}</h4>
-                        <img src=${e.image}>
-                        <details>
-                            <summary>Details</summary>
-                            <p>${e.description}</p>
-                        </details>`
-                // let img = document.createElement('img');
-                // let p = document.createElement('p');
-                // img.src = e.image
-                // p.innerHTML = `<p> NAME: ${e.name} </p>`
-                document.querySelector(".gallery").appendChild(div);
-              })                              
-          })
-          .catch(error => {
-            alert("Error!");
-            let p = document.createElement('p');
-            p.innerHTML = `ERROR: ${error.message}`
-            document.body.appendChild(p);
-          })     
+           
+            if (searchCategory){
+                document.getElementsByTagName("button")[0].removeAttribute("disabled")
+            }
+            // else {
+            //     document.getElementsByTagName("button").setAttribute("disabled")
+            // }
+            // console.log("BUTTON: " + document.getElementsByTagName("button"))
         })
 
 
@@ -50,15 +31,21 @@
         searchForm.addEventListener('submit', (event) => {
           event.preventDefault();
           const searched = event.target.search.value;
+
       
           console.log(event.target)
           console.log(searched);
           console.log("CHECK#2 " + searchCategory)
+          
       
-          fetch(`https://eldenring.fanapis.com/api/${searchCategory}?name=${searched}`)
+          const path = !searched ? `${searchCategory}?limit=500` : `${searchCategory}?name=${searched}`
+               
+          fetch(`https://eldenring.fanapis.com/api/${path}`)
           .then(response => response.json())
           .then(object => { 
+              document.querySelector(".gallery").replaceChildren();
               console.log(object)
+              console.log(`https://eldenring.fanapis.com/api/${path}`)
               object.data.forEach(e => {
                 const div = document.createElement('div');
                 div.innerHTML = `
@@ -78,7 +65,7 @@
           .catch(error => {
             alert("Error!");
             let p = document.createElement('p');
-            p.innerHTML = `ERROR: ${error.message}`
+            //p.innerHTML = `ERROR: ${error.message}`
             document.body.appendChild(p);
           })          
         });
